14273ac50c2fd42f9f5f7fc5a3d6d619079bd94e
Kiyoshi Masui <kiyo@physics.ubc.ca>
2015-08-31 00:48:59 -0400

    Many bug fixes.

--- aro_tools/power_data_io.py
+++ aro_tools/power_data_io.py
@@ -1,8 +1,9 @@
+import numpy as np
 
 # File format parameters
 HEADER_LEN = 12       # Bytes: index, loops, n_FFT_int
 FRAME_HEADER_LEN = 8  # Bytes: FGPA counts, Unix_time
-DTYPE = np.uint32
+DTYPE = np.dtype(np.uint32)
 
 # Acquisition parameters
 NTIME_RECORD = 1024     # Post integration, post-scrunch. Arbitrary, doesn't matter
@@ -17,19 +18,15 @@ NPOL = 2
 CAL_PERIOD_SAMPLES = 0
 
 
-# Run parameters:
 
-SAFETY_INTEG = 2048
-
-
-class PowerDataRing(object):
+class Ring(object):
 
     def __init__(self, filename, scrunch=1):
 
         self._filename = filename
         self._scrunch = scrunch
 
-        with open(filename, r) as f:
+        with open(filename, 'r') as f:
             s = f.read(HEADER_LEN)
             f.seek(0, 2)
             size = f.tell()
@@ -41,21 +38,23 @@ class PowerDataRing(object):
             raise RuntimeError('Odd filesize.')
 
         header = np.fromstring(s, DTYPE)
-        self._sample_time = arr[2] / FFT_RATE
+        self._sample_time = header[2] / FFT_RATE
         self._delta_t = self._sample_time * scrunch
-        self._record_offset = ((arr[0] + arr[1] * self._ninteg_ring)
+        # The record we will call "0".
+        self._record_offset = ((header[0] + (header[1]) * self._ninteg_ring)
                                // (scrunch * NTIME_RECORD))
 
 
     def current_records(self):
         ninteg_ring = self._ninteg_ring
-        with open(self._filename, r) as f:
+        with open(self._filename, 'r') as f:
             s = f.read(HEADER_LEN)
         header = np.fromstring(s, DTYPE)
         index = header[0]
         loops = header[1]
         last_record = ((index + loops * ninteg_ring)
-                       // (self._scrunch * NTIME_RECORD)
+                       // (self._scrunch * NTIME_RECORD) + 1)
+        last_record -= self._record_offset
         nrecord_ring = ninteg_ring // (self._scrunch * NTIME_RECORD)
         # The latest 80% of the ring is availble, for safety.
         first_available_record = last_record - int(nrecord_ring * 0.8)
@@ -74,8 +73,54 @@ class PowerDataRing(object):
 
         parameters['delta_f'] = DELTA_F
         parameters['ntime_record'] = NTIME_RECORD
-        parameters['nrecords'] = self._get_nrecords(filename)
+        parameters['nrecords'] = self.current_records()[1]
 
         return parameters
+
+
+    def read_records(self, start_record=None, end_record=None):
+
+        first_available_record, last_record = self.current_records()
+
+        if start_record is None:
+            start_record = first_available_record
+        if end_record is None:
+            end_record = last_record
+
+        if start_record < first_available_record:
+            raise DataGone()
+        if end_record > last_record:
+            end_record = last_record
+        if start_record >= last_record:
+            raise ValueError("No data to read.")
+
+        scrunch = self._scrunch
+
+        out = np.empty((NFREQ, (end_record - start_record), NTIME_RECORD),
+                       dtype=np.float32)
+        integ_bytes = NPOL * NFREQ * DTYPE.itemsize
+
+        with open(self._filename, 'r') as f:
+            for ii in range(end_record - start_record):
+                abs_record_ind = ii + start_record + self._record_offset
+                record_data = np.zeros((NTIME_RECORD, NFREQ), dtype=np.float32)
+                for jj in range(NTIME_RECORD):
+                    integ_ind_jj = (abs_record_ind * NTIME_RECORD + jj) * scrunch
+                    for kk in range(self._scrunch):
+                        integ_ind = (integ_ind_jj + kk) % self._ninteg_ring
+                        f.seek(HEADER_LEN + integ_ind * integ_bytes)
+                        s = f.read(integ_bytes)
+                        integ = np.fromstring(s, dtype=DTYPE)
+                        integ.shape = (NPOL, NFREQ)
+                        for pp in range(NPOL):
+                            record_data[jj, :] += integ[pp]
+                out[:,ii,:] = np.transpose(record_data)
+        out.shape = (NFREQ, (end_record - start_record) * NTIME_RECORD)
+        return out
+
+
+
+class DataGone(Exception):
+    pass
 
 
